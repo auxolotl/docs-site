a1e9459a60f004d32063b9132a8ef3a5c877cabc
Skyler Grey <minion@clicks.codes>
2024-05-17 00:01:10 +0000

    feat: Add project configuration
    We're setting this up as an astro+bun project, and we're using a flake
    to let us install bun with direnv.
    
    We also added .gitmodules to pull in our wiki content

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A Clicks project";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { packages = [ pkgs.bun ]; };
+	      formatter = pkgs.nixfmt;
+      }
+    );
+}
